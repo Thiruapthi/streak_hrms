86f28e5eb6362b373bb45023de40b38d8891fdca
umakanth1999 <kaspaumakanth1999@gmail.com>
2023-10-27 15:28:44 +0530

    show this interview option only after an interview has been created for the applicant

--- hyde_app/public/js/job_applicant.js
+++ hyde_app/public/js/job_applicant.js
@@ -3,6 +3,26 @@ frappe.ui.form.on("Job Applicant", {
     refresh: function(frm) {
         var interviewButton = $(".document-link[data-doctype='Interview'] button[data-doctype='Interview']");
         interviewButton.remove();
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+              doctype: "Interview",
+              filters: {
+                job_applicant: frm.doc.name,
+              },
+            },
+            callback: function (response) {
+              if (!response.exc) {
+                console.log(response.message.length);
+                if (response.message.length == 0) {
+                  console.log(response.message.length);
+                  $(".document-link[data-doctype='Interview']").hide();
+                } else {
+                  $(".document-link[data-doctype='Interview']").show();
+                }
+              }
+            },
+          });
     },
     before_save(frm) {
         console.log("hello")
