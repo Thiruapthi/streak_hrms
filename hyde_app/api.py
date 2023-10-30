5d0fecb8b60381d56640e3b5df5c9f64c6cb0466
Kamal Singh <karan.s@korecent.com>
2023-10-30 12:07:46 +0530

    delete property setter from fixtures and added reload doc method

--- hyde_app/api.py
+++ hyde_app/api.py
@@ -6,6 +6,7 @@ def update_applicant_status_interview(applicant_name, status):
         applicant = frappe.get_doc("Job Applicant", applicant_name)
         applicant.status = status
         applicant.save()
+        applicant.reload()
         frappe.db.commit()
     except frappe.DoesNotExistError:
         frappe.msgprint(f"Job Applicant '{applicant_name}' not found.")
