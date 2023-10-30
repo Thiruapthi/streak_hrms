372e7fd1fb6b952a38e4126fef2b8cfcb3aa49f0
lavaraju97 <lavaraju@korecent.com>
2023-10-30 16:52:34 +0530

    fixed minor issues in Weekly Attendance Sheet

--- hyde_app/www/weekly_attendance_sheet/index.js
+++ hyde_app/www/weekly_attendance_sheet/index.js
@@ -3,8 +3,6 @@ frappe.ready(function(){
 	if(frappe.session.user === "Guest"){
 		const heading = document.getElementById("heading")
 		const filtersContainer = document.getElementById("filters-container")
-		console.log(frappe.session.user)
-
 		heading.textContent = "You can't see the Report without Login"
 		heading.classList.add("heading-without-login")
 		filtersContainer.textContent = ""
@@ -14,7 +12,6 @@ frappe.ready(function(){
 	else{
 
 		function displayTableWithData(columns, data, present_date, previous_date) {
-			console.log("1",columns,data,present_date,previous_date)
 			var table = document.getElementById("data-table");
 			var previous_dateEl = document.getElementById("previous_date");
 			var present_dateEl = document.getElementById("present_date");
@@ -23,7 +20,7 @@ frappe.ready(function(){
 			while (table.rows.length > 0) {
 				table.deleteRow(0); // Deletes the first row in the table
 			}
-		
+
 			// Add table header columns dynamically
 			var thead = table.createTHead();
 			var row = thead.insertRow();
@@ -87,73 +84,137 @@ frappe.ready(function(){
 				}
 			}
 		}
-			var employeeEl = document.getElementById("employee-input")
-			var buttonEl = document.getElementById("button")
-			var employeeName = ""
-			var employeeId = ""
-			buttonEl.addEventListener("click",function(){
-				employeeName = employeeEl.value
-				console.log(employeeName)
-				employeeEl.value = ""
-				frappe.call({
-					method:"hyde_app.www.weekly_attendance_sheet.index.get_employee_id",
-					args:{inputName:employeeName},
-					callback:function(r){
-						employeeId = r.message
-						console.log(employeeId)
 
-						if (employeeId===undefined){
-							frappe.throw(
-								title='Enter Valid Employee Name'
-								)
-						
-						}
-						frappe.call({
-							method: "hyde_app.www.weekly_attendance_sheet.index.get_weekly_report",
-							args:{
-							   'company': 'KCS','employee':employeeId
-					
-							},
-							callback: function(r) {
-					
-								var columns = r.message[0];
-								var data = r.message[1];
-								var present_date = r.message[2]
-								var previous_date = r.message[3]
-								
-								console.log("2",columns,data,present_date,previous_date)
-								displayTableWithData(columns, data, present_date, previous_date)
-					
-					
-							}
-						});
+
+		var defaultCompany = 'Korecent Solutions';
+		var defaultEmployee = '';
 		
+		frappe.call({
+			method: "hyde_app.www.weekly_attendance_sheet.index.get_employees",
+			callback: function (r) {
+				var employeeList = r.message
+				var employeeEl = document.getElementById("employee");
+
+				for (var i = 0; i < employeeList.length; i++) {
+					var option = document.createElement("option");
+					option.value = employeeList[i].name;
+					option.text = employeeList[i].name + ' - ' + employeeList[i].employee_name;
+					employeeEl.appendChild(option);
+				}
+			}
+		})
+
+		frappe.call({
+			method: "hyde_app.www.weekly_attendance_sheet.index.get_company_list",
+			callback: function (r) {
+				var employeeList = r.message
+				var companyEl = document.getElementById("company");
+
+				for (var i = 0; i < employeeList.length; i++) {
+					var option = document.createElement("option");
+					option.value = employeeList[i].name;
+					option.text = employeeList[i].name
+					companyEl.appendChild(option);
+				}
+
+		    var companyEl = document.getElementById('company');
+		    companyEl.value = defaultCompany;
+			}
+		})		
+
+		frappe.call({
+
+			method: "hyde_app.www.weekly_attendance_sheet.index.get_weekly_report",
+			args: {
+				
+				'employee': defaultEmployee,
+				'company': defaultCompany
+
+			},
+
+			callback: function (r) {
+				var columns = r.message[0];
+				var data = r.message[1];
+				var present_date = r.message[2]
+				var previous_date = r.message[3]							
+				displayTableWithData(columns, data, present_date, previous_date)
+		
+			}
+		});
+		
+		document.getElementById('company').addEventListener('change', applyFilter);
+
+		let employeeEl = document.getElementById("employee")
+
+		employeeEl.addEventListener("change", () => {
+			fetchCompanyName()
+			setTimeout(() => {
+
+				let employeeVal = document.getElementById('employee').value;
+				let companyVal = document.getElementById('company').value;
+				
+				frappe.call({
+					method: "hyde_app.www.weekly_attendance_sheet.index.get_weekly_report",
+					args: {
+						'employee': employeeVal,
+						'company': companyVal
+					},
+					callback: function (r) {
+						var columns = r.message[0];
+						var data = r.message[1];
+						var present_date = r.message[2]
+						var previous_date = r.message[3]							
+						displayTableWithData(columns, data, present_date, previous_date)
 					}
 				});
-				
-			});
+
+			}, 1000);
+
+
+		})
+
+		function fetchCompanyName() {
+
+			frappe.call({
+				method:"hyde_app.www.weekly_attendance_sheet.index.get_employees",
+				callback: function (r) {
+					var employeeList = r.message
+					
+					for (let eachEmployeeItem of employeeList) {
+						if (eachEmployeeItem.name === employeeEl.value) {
+							let company = eachEmployeeItem.company
+
+							var companyEl = document.getElementById("company")
+							companyEl.value = company
+						}
+					}
+
+				}
+			})
+
+		}
 		
-		
+		function applyFilter() {
+
+			let employeeVal = document.getElementById('employee').value;
+			let companyVal = document.getElementById('company').value;
+
 			frappe.call({
 				method: "hyde_app.www.weekly_attendance_sheet.index.get_weekly_report",
-				args:{
-				   'company': 'KCS','employee':employeeId
-		
+				args: {
+					'employee': employeeVal,
+					'company': companyVal
 				},
-				callback: function(r) {
-
+				callback: function (r) {
 					var columns = r.message[0];
 					var data = r.message[1];
 					var present_date = r.message[2]
-					var previous_date = r.message[3]
-					console.log("3",columns, data, present_date, previous_date)
-							
+					var previous_date = r.message[3]							
 					displayTableWithData(columns, data, present_date, previous_date)
-		
-		
+					
 				}
 			});
-
+		}
 
 	}
 	
--- hyde_app/www/weekly_attendance_sheet/index.py
+++ hyde_app/www/weekly_attendance_sheet/index.py
@@ -4,17 +4,9 @@ from hrms.hr.report.monthly_attendance_sheet.monthly_attendance_sheet import exe
 
 @frappe.whitelist(allow_guest=True)
 def get_weekly_report(company,employee):
-
     current_date = date.today()
     current_month = int((current_date.strftime("%Y-%m-%d")).split("-")[1])
     current_year = int((current_date.strftime("%Y-%m-%d")).split("-")[0])
-
-    frappe.log_error("curent_date",current_date)
-    frappe.log_error("current_month",current_month)
-    frappe.log_error("current_year",current_year)
-    frappe.log_error("company",company)
-    frappe.log_error("employee",employee)
-
 
 
     filters ={
@@ -22,9 +14,6 @@ def get_weekly_report(company,employee):
 
 		}
     columns, data, message, chart = execute(filters)
-
-    frappe.log_error("columns",data)
-    frappe.log_error("columns",columns)
 
     updatedcolumns = []
     updateddata = []
@@ -34,19 +23,14 @@ def get_weekly_report(company,employee):
     previous_start_date = current_date+timedelta(days=-6)
     previous_start_date_num = int((previous_start_date.strftime("%Y-%m-%d")).split("-")[2])
 
-    frappe.log_error("current_date_num",current_date_num)
-    frappe.log_error("previous_start_date", previous_start_date)
-    frappe.log_error("previous_start_date_num",previous_start_date_num)
-
-
-
     for eachcolumn in columns:
         if (type(eachcolumn["fieldname"])==int):
             if previous_start_date_num<=eachcolumn["fieldname"]<=current_date_num:
                 updatedcolumns.append(eachcolumn)
 
         elif (type(eachcolumn["fieldname"])==str):
-            updatedcolumns.append(eachcolumn)
+            if(eachcolumn["label"]!="Shift"):
+                updatedcolumns.append(eachcolumn)
 
 
     for eachrow in data:
@@ -68,21 +52,14 @@ def get_weekly_report(company,employee):
     present_date = present_date.strftime("%d-%m-%Y")
     previous_date = previous_date.strftime("%d-%m-%Y")
 
-    
-    frappe.log_error("updatedColumns",updatedcolumns)
-    frappe.log_error("updatedData",updateddata)
-    frappe.log_error("present_date", present_date)
-    frappe.log_error("previous_date",previous_date)
-
     return updatedcolumns,updateddata,present_date,previous_date
 
-    
 @frappe.whitelist(allow_guest=True)
-def get_employee_id(inputName):
+def get_employees():
+	employeeList = frappe.get_all('Employee',filters={'status':'Active'},fields=['name', 'employee_name','company','status'])
+	return employeeList
 
-    frappe.log_error("inputName")
-    employees_data = frappe.get_all("Employee", fields = '*')
-    for each_employee_data in employees_data:
-        if(inputName.lower()==each_employee_data.first_name.lower()):
-            frappe.log_error("each_employee_Id",each_employee_data.name)
-            return (each_employee_data.name)
+@frappe.whitelist(allow_guest=True)
+def get_company_list():
+	companyList = frappe.get_all('Company')
+	return companyList
