bac69cef6eafdc2fa537b0b0f1ca724f1c5b3c7d
Hyde19 <hyde.k@korecent.com>
2023-11-03 14:46:48 +0530

    setting leave approver name in leave application

--- hyde_app/public/js/leave_application.js
+++ hyde_app/public/js/leave_application.js
@@ -10,5 +10,22 @@ frappe.ui.form.on('Leave Application', {
                 });
             } 
         });
-    }
+    },
+
+    leave_approver: function(frm) {
+		if (frm.doc.leave_approver) {
+            frappe.call({
+                method:'hyde_app.api.get_user_details',
+                args: {
+                    user:frm.doc.leave_approver
+                },
+                callback: function(r){ 
+                    if(r.message){
+                        var full_name = r.message[0].full_name
+                    	frm.set_value("leave_approver_name", full_name)
+                    }
+                }
+            })
+		}
+	}
 });
