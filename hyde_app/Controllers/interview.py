06895fc57086018792e33a73aeb7d6bff9f46159
lavaraju97 <lavaraju@korecent.com>
2023-11-06 17:18:25 +0530

    If Interview is Online then send meet link to both Interviewer and Job applicant

--- hyde_app/Controllers/interview.py
+++ hyde_app/Controllers/interview.py
@@ -19,65 +19,119 @@ class CustomInterviewController(OriginalInterviewController):
         Schedule_On = self.scheduled_on
         from_time = self.from_time
         Interview_round = self.interview_round
+        interview_type = self.custom_interview_type
+        interview_link = self.custom_interview_link
         Schedule_On_date = getdate(Schedule_On)
         
         
         Interviewer_Name = self.interview_details[0].custom_interviewer_name
-        Interviewer_Email = self.interview_details[0].interviewer
+        Interviewer_Emails_List = self.interview_details
+        interviewer_emails = []
+        for each_interviewer in Interviewer_Emails_List:
+            interviewer_emails.append(each_interviewer.interviewer)
+        print(interviewer_emails)
         dynamic_link = self.resume_link
-        
-        if Schedule_On_date <= next_day:
-            Record_Name = self.name
-            message = (
-				_("Dear {0},").format(job_Applicant)
-				+ "<br><br>"
-				+ _("Greetings of the day!")
-				+ "<br><br>"
-				+ _("We are pleased to inform you that your Interview is scheduled for  {0} on {1} at {2}").format(designation,Schedule_On,from_time)
-				+"<br><br>"
-				+_("It will be a {0}").format(Interview_round)
-				+"<br><br>"
-				+_("Wishing you the best for your Interview.")
-				+"<br><br>"
-				+_("Thanks and regards,")
-				+"<br><br>"
-				+_("HR-Team KoreCent")
-			)
-            
-            frappe.sendmail(
-				recipients=job_Applicant_Email,
-				subject="Interview Remainder",
-				message=message,
-				reference_doctype="Interview",
-				reference_name=Record_Name,
-			)
-            
-            Interviewer_message = (
-				_("Dear {0},").format(Interviewer_Name)
-				+"<br><br>"
-				+_("Greetings of the day!")
-				+"<br><br>"
-				+_("An Interview has been scheduled for  {0} on {1} at {2}. Candidate name is {3}.").format(designation,Schedule_On,from_time,job_Applicant)
-				+"<br><br>"
-				+_("Below enclosed is the resume for your ")
-				+f'<a href="{dynamic_link}"> Reference</a>'
-				+"<br><br>"
-				+_("Please ensure your availability for the Interview and incase of any rescheduling let us know 1 day in advance.")
-				+"<br><br>"
-				+_("Thanks and regards")
-				+"<br><br>"
-				+_("HR- Team KoreCent")
-			)
-            
-            Interviewer_Subject = "Software Engineer interview " + Interview_round
-            
-            frappe.sendmail(
-				recipients=Interviewer_Email,
-				subject=Interviewer_Subject,
-				message=Interviewer_message,
-				reference_doctype="Interview",
-				reference_name=Record_Name,
-			)
-
-
-
+        if interview_type == "Offline":
+            if Schedule_On_date <= next_day:
+                Record_Name = self.name
+                message = (
+					_("Dear {0},").format(job_Applicant)
+					+ "<br><br>"
+					+ _("Greetings of the day!")
+					+ "<br><br>"
+					+ _("We are pleased to inform you that your Interview is scheduled for  {0} on {1} at {2}").format(designation,Schedule_On,from_time)
+					+"<br><br>"
+					+_("It will be a {0}").format(Interview_round)
+					+"<br><br>"
+					+_("Wishing you the best for your Interview.")
+					+"<br><br>"
+					+_("Thanks and regards,")
+					+"<br><br>"
+					+_("HR-Team KoreCent")
+				)
+                frappe.sendmail(
+					recipients=job_Applicant_Email,
+					subject="Interview Remainder",
+					message=message,
+					reference_doctype="Interview",
+					reference_name=Record_Name,
+				)
+                Interviewer_message = (
+					_("Dear {0},").format(Interviewer_Name)
+					+"<br><br>"
+					+_("Greetings of the day!")
+					+"<br><br>"
+					+_("An Interview has been scheduled for  {0} on {1} at {2}. Candidate name is {3}.").format(designation,Schedule_On,from_time,job_Applicant)
+					+"<br><br>"
+					+_("Below enclosed is the resume for your ")
+					+f'<a href="{dynamic_link}"> Reference</a>'
+					+"<br><br>"
+					+_("Please ensure your availability for the Interview and incase of any rescheduling let us know 1 day in advance.")
+					+"<br><br>"
+					+_("Thanks and regards")
+					+"<br><br>"
+					+_("HR- Team KoreCent")
+				)
+                Interviewer_Subject = "Software Engineer interview " + Interview_round
+                
+                frappe.sendmail(
+					recipients=interviewer_emails,
+					subject=Interviewer_Subject,
+					message=Interviewer_message,
+					reference_doctype="Interview",
+					reference_name=Record_Name,
+				)
+        else:
+            if Schedule_On_date <= next_day:
+                Record_Name = self.name
+                message = (
+					_("Dear {0},").format(job_Applicant)
+					+ "<br><br>"
+					+ _("Greetings of the day!")
+					+ "<br><br>"
+					+ _("We are pleased to inform you that your Interview is scheduled for  {0} on {1} at {2}").format(designation,Schedule_On,from_time)
+					+"<br><br>"
+					+_("It will be a {0}").format(Interview_round)
+					+"<br><br>"
+                    +_("Interview Link: {0}").format(interview_link)
+                    +"<br><br>"
+					+_("Wishing you the best for your Interview.")
+					+"<br><br>"
+					+_("Thanks and regards,")
+					+"<br><br>"
+					+_("HR-Team KoreCent")
+				)
+                frappe.sendmail(
+					recipients=job_Applicant_Email,
+					subject="Interview Remainder",
+					message=message,
+					reference_doctype="Interview",
+					reference_name=Record_Name,
+				)
+                Interviewer_message = (
+					_("Dear {0},").format(Interviewer_Name)
+					+"<br><br>"
+					+_("Greetings of the day!")
+					+"<br><br>"
+					+_("An Interview has been scheduled for  {0} on {1} at {2}. Candidate name is {3}.").format(designation,Schedule_On,from_time,job_Applicant)
+					+"<br><br>"
+					+_("Below enclosed is the resume for your ")
+					+f'<a href="{dynamic_link}"> Reference</a>'
+					+"<br><br>"
+                    +_("Interview Link: {0}").format(interview_link)
+                    +"<br><br>"
+					+_("Please ensure your availability for the Interview and incase of any rescheduling let us know 1 day in advance.")
+					+"<br><br>"
+					+_("Thanks and regards")
+					+"<br><br>"
+					+_("HR- Team KoreCent")
+				)
+                Interviewer_Subject = "Software Engineer interview " + Interview_round
+                
+                frappe.sendmail(
+					recipients=interviewer_emails,
+					subject=Interviewer_Subject,
+					message=Interviewer_message,
+					reference_doctype="Interview",
+					reference_name=Record_Name,
+				)
