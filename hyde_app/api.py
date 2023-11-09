8821928b133c32f5d914f518e6413080ba973865
umakanth1999 <umakanth@korecent.com>
2023-11-09 11:17:19 +0530

    Added code for sending appointment letters, offer letters, and emails to HR when candidates complete all rounds

--- hyde_app/api.py
+++ hyde_app/api.py
@@ -142,4 +142,225 @@ def change_interview_status(interview_id,interviewer_list):
             frappe.db.commit()
     except Exception as e:
         print(f"Error occured : {e}")
-    
+
+import frappe
+@frappe.whitelist(allow_guest=True)
+def get_job_applications(email):
+   job_applications = frappe.get_all(
+       'Job Applicant',
+       filters={
+           'email_id': email
+       },
+       fields=['name', 'job_title', 'status', 'applicant_name', 'applicant_rating']
+   )
+   for application in job_applications:
+       application['interviews'] = get_interviews(application['name'])
+
+
+   return job_applications
+
+
+def get_interviews(applicant_name):
+   interviews = frappe.get_all(
+       'Interview Feedback',
+       filters={
+           'job_applicant': applicant_name
+       },
+       fields=['interview_round', "interviewer",'result','feedback']
+   )
+
+
+   return interviews
+
+@frappe.whitelist()
+def send_appointment_email(doc, method):
+    print(frappe.utils.get_url())
+    # Get multiple values from the document based on the job applicant's name
+    applicant_data = frappe.get_value(
+        "Job Applicant",
+        {"name": doc.job_applicant},
+        ["email_id", "job_title", "applicant_name"],
+        as_dict=True
+    )
+
+    # Access individual values as needed
+    applicant_email = applicant_data.get("email_id")
+    applicant_name = applicant_data.get("applicant_name")
+    position = applicant_data.get("job_title")
+    # ctc = doc.ctc
+
+    # Compose the HTML email content using the provided template and dynamic values
+    email_content = f"""\
+        <html>
+        <head>
+            <style>
+                /* Add your styling here */
+            </style>
+        </head>
+        <body>
+            <p>Dear {applicant_name},</p>
+            <p>Greetings of the day!</p>
+            <p>Heartiest congratulations! We are pleased to offer you the position of {position} Business Analyst with Korecent Solutions Pvt. Ltd. at {1} CTC.</p>
+            <p>Please take the necessary action on the below enclosed link within the next 7 days:</p>
+               <a href="{ frappe.utils.get_url() }/api/method/hyde_app.api.accept_offer?name={applicant_email }" 
+   onclick="this.style.pointerEvents = 'none'; this.style.background = '#ccc';" 
+   style="background-color: #4CAF50; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block; border-radius: 5px; margin-right: 10px;">Accept</a>
+
+<a href="{ frappe.utils.get_url() }/api/method/hyde_app.api.reject_offer?name={ applicant_email }" 
+   onclick="this.style.pointerEvents = 'none'; this.style.background = '#ccc';" 
+   style="background-color: #FF5733; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block; border-radius: 5px;">Reject</a>
+    </div>
+            <p>Thanks and regards,</p>
+            <p>HR- Team KoreCent</p>
+        </body>
+        </html>
+    """
+
+    # Send HTML email
+    frappe.sendmail(
+        recipients=applicant_email,
+        cc='oorjaa@korecent.com',
+        subject='Job Offer Rejected',
+        message=email_content,
+        now=True
+    )
+
+
+
+
+
+
+#code with dummy data
+
+@frappe.whitelist(allow_guest=True)
+def accept_offer():
+  subject = 'Job Offer Accepted'
+   # Create the HTML content for the email
+  email_content = """
+  <html>
+  <head>
+      <title>Job Offer Accepted</title>
+  </head>
+  <body>
+      <p>Dear Recipient,</p>
+      <p>We are pleased to inform you that your acceptance of the job offer for the position of Frappe Developer at KCS has been received.</p>
+      <p>Please find attached the formal offer letter. Kindly review it for further instructions.</p>
+      <p>Thank you for choosing to join our team.</p>
+      <p>Best regards,</p>
+      <p>Your Name</p>
+  </body>
+  </html>
+  """
+  frappe.sendmail(
+      recipients = 'umakanth@korecent.com',
+      subject = subject,
+      message=email_content,
+      attachments=[{"file_url": "/files/artturi-jalli-g5_rxRjvKmg-unsplash.jpg"}],
+      now = True
+  )
+  # Create an HTML page response
+  page_content = """
+  <html>
+  <head>
+      <title>Offer Accepted</title>
+  </head>
+  <body>
+      <h1>Thanks for accepting the offer!</h1>
+      <p>You will receive the offer letter soon.</p>
+  </body>
+  </html>
+  """
+
+
+
+
+  return frappe.respond_as_web_page("Offer Accepted", page_content)
+
+
+
+
+@frappe.whitelist(allow_guest=True)
+def reject_offer():
+  frappe.sendmail(
+      recipients='umakanth@korecent.com',
+      subject='Job Offer Rejected',
+      message='Your job offer has been rejected.',
+      now = True
+  )
+  frappe.sendmail(
+      recipients='oorjaa@korecent.com',
+      subject='Job Offer Rejected',
+      message='Your job offer has been rejected.',
+      now = True
+  )
+  # Create an HTML page response
+  page_content = """
+  <html>
+  <head>
+      <title>Offer Rejected</title>
+  </head>
+  <body>
+      <h1>Offer Rejected</h1>
+      <p>Thank you for considering our offer. If you change your mind, please feel free to contact us.</p>
+  </body>
+  </html>
+  """
+
+
+
+
+  return frappe.respond_as_web_page("Offer Rejected", page_content)
+
+
+
+
+
+
+
+@frappe.whitelist()
+def notify_hr_on_interview_update(doc, method):
+  if  doc.status =='Cleared':
+      job_opening = frappe.get_doc("Job Opening", doc.job_opening)
+      all_rounds_cleared = True
+      for i in job_opening.custom_round:
+          sql_query = f"""
+              SELECT
+                  *
+              FROM
+                  `tabInterview`
+              WHERE
+                  `job_applicant` = %s
+                  AND `interview_round` = %s
+              ORDER BY
+                  `creation` DESC
+              LIMIT 1
+          """
+
+
+
+
+          interview_record = frappe.db.sql(sql_query, (doc.job_applicant, i.interview_rounds), as_dict=True)
+
+
+
+
+          if interview_record:
+              if interview_record[0].status != "Cleared":
+                  all_rounds_cleared = False
+          else:
+              all_rounds_cleared = False
+
+
+
+
+      if all_rounds_cleared:
+          recipient_email = 'umakanth@korecent.com'
+          subject = 'Job Offer Approval'
+          message = f'All interview rounds have been cleared for the candidate {doc.job_applicant}. Please proceed to release the job offer.'
+          frappe.sendmail(
+          recipients=[recipient_email],
+          subject=subject,
+          message=message,
+          now=True,
+          )
+
--- hyde_app/public/js/job_applicant.js
+++ hyde_app/public/js/job_applicant.js
@@ -1,5 +1,15 @@
 frappe.ui.form.on("Job Applicant", {
   refresh: function (frm) {
+    frm.add_custom_button('Applicant History', function(){
+      frappe.call({
+        args: {email:frm.doc.email_id},
+        method: "hyde_app.api.get_job_applications",
+        callback: function(r) {
+          if (r.message) {
+            showJobApplications(r.message);
+        }  }
+    });
+    });
     
     cur_frm.events.create_dialog = function (frm) {
      
@@ -154,3 +164,117 @@ frappe.ui.form.on("Job Applicant", {
     
 	}
 })
+
+
+function showJobApplications(data) {
+  console.log(data);
+  let d = new frappe.ui.Dialog({
+    title: "Job Applications",
+    fields: [
+      {
+        label: "Job Applications",
+        fieldname: "history",
+        fieldtype: "HTML",
+        options: createItemTable(data),
+        read_only: 1,
+      },
+    ],
+    size: "large",
+    primary_action_label: "Submit",
+    primary_action: function (values) {
+      console.log(values);
+      d.hide();
+    },
+  });
+
+  d.show();
+  setTimeout(function () {
+    data.forEach((item) => {
+      if (item.interviews.length === 0) {
+        console.log(item.name, "test");
+
+        $(`button[data-target="${item.name}"]`).hide();
+      }
+    });
+  }, 500);
+}
+
+function createItemTable(data) {
+  let tableHTML = `
+      <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th class="table-heading">#</th>
+            <th class="table-heading">Applicant Name</th>
+            <th class="table-heading">Job Title</th>
+            <th class="table-heading">Status</th>
+            <th class="table-heading">Applicant Rating</th>
+            <th class="table-heading">Interview Feedback</th>
+          </tr>
+        </thead>
+        <tbody>
+    `;
+
+  data.forEach((item, index) => {
+    tableHTML += `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${item.applicant_name}</td>
+          <td>${item.job_title}</td>
+          <td>${item.status}</td>
+          <td>${item.applicant_rating}</td>
+          <td><button class="show-interview-button btn btn-primary" data-target="${
+            item.name
+          }">Show</button></td>
+        </tr>
+        <tr id="${item.name}" style="display: none;">
+          <td colspan="6">
+            <table class="interview-table">
+              <thead>
+                <tr>
+                  <th>Interview Round</th>
+                  <th>Interviewer</th>
+                  <th>Result</th>
+                  <th>Feedback</th>
+                </tr>
+              </thead>
+              <tbody>
+                ${item.interviews
+                  .map(
+                    (interview) => `
+                      <tr>
+                        <td>${interview.interview_round}</td>
+                        <td>${interview.interviewer}</td>
+                        <td>${interview.result}</td>
+                        <td>${interview.feedback}</td>
+                      </tr>
+                    `
+                  )
+                  .join("")}
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      `;
+  });
+
+  tableHTML += `
+        </tbody>
+      </table>
+    `;
+
+  return tableHTML;
+}
+
+$(document).on("click", ".show-interview-button", function () {
+  const target = $(this).data("target");
+  var trElement = document.getElementById(target);
+
+  if (trElement) {
+    if (trElement.style.display === "none" || trElement.style.display === "") {
+      trElement.style.display = "table-row";
+    } else {
+      trElement.style.display = "none";
+    }
+  }
+});
