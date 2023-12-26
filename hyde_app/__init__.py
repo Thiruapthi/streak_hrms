110dd7bda8c0600fbb96239aafd063c0c7915fa3
Abhimanyu <abhimanyu.d@korecent.com>
2023-12-26 18:49:33 +0530

    auto creation attendance on applied leaves

--- hyde_app/__init__.py
+++ hyde_app/__init__.py
@@ -2,5 +2,9 @@
 __version__ = '0.0.1'
 
 from hrms.hr.doctype.interview.interview import Interview
+from hrms.hr.doctype.attendance.attendance import Attendance
 from hyde_app.api import set_average_rating
+from hyde_app.api import mark_attendance_for_leave
 Interview.set_average_rating = set_average_rating
+
+Attendance.validate_attendance_date=mark_attendance_for_leave
